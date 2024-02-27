86c09da5723e63e32d67316120d63a7a994d776d
rizk <rizkdotai@gmail.com>
2024-02-27 19:28:02 +0200

    add responsive form

---
+++ src/app/form-generator/index.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, {useState} from 'react'
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter
+  } from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea';
+
+type Props = {}  
+
+const FormGenerator = (props: Props) => {
+
+    const [open, setOpen] = useState(false);
+   
+    const onFormCreate = () => {
+        setOpen(true);
+    }
+    
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+  <Button onClick={onFormCreate}>Create Form</Button>
+  <DialogContent className='sm:max-w-[425px]'>
+    <DialogHeader>
+      <DialogTitle>Create New Form</DialogTitle>
+      <form>
+        <div className='grid gap-4 py-4'>
+            <Textarea id='description' name='description' required placeholder='Share what your form is about, who is it for, and what information you would like to collect. And AI will do the magic ✨'/>
+        </div>
+      </form>
+    </DialogHeader>
+    <DialogFooter>
+        <Button variant="link">Create Manually</Button>
+    </DialogFooter>
+  </DialogContent>
+</Dialog>
+
+  )
+}
+
+export default FormGenerator
