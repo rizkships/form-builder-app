5c150a5302485a98fcd249ee4b16f98d877f7166
rizk <rizkdotai@gmail.com>
2024-03-09 16:43:57 +0200

    connect drizzle

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,28 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-export const { handlers, auth, signIn, signOut } = NextAuth({ providers: [ GoogleProvider({
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./db/index";
+
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+  } = NextAuth({
+    adapter: DrizzleAdapter(db),
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        })
-    ]
-}) ]})
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+    ],
+    callbacks: {
+      async session({ session, user }: { session: Session; user?: User }) {
+        if (user && session?.user) {
+          session.user.id = user.id;
+        }
+        return session;
+      },
+    },
+  });
